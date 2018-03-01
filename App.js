353faa3b6716750e2eb61210fe1a9fefb662ac46
Janne Hellsten <jjhellst@gmail.com>
2018-03-02 01:08:01 +0200

    build #2 with different background color

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#42f',
     alignItems: 'center',
     justifyContent: 'center',
   },
