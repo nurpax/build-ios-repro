f36bc567b1c237055785209babc8b9a86a207340
Janne Hellsten <jjhellst@gmail.com>
2018-03-02 11:28:14 +0200

    build #4 with pink background

--- App.js
+++ App.js
@@ -20,7 +20,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f77',
     alignItems: 'center',
     justifyContent: 'center',
   },
